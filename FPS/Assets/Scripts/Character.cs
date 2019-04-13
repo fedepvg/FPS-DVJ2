77d16fff7f9ce501aef30934e3c3880a7d28358c
fedepvg <federicopvg@gmail.com>
2019-04-13 19:52:45 -0300

    FPS character created

---
+++ FPS/Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int health=100;
+    private Rigidbody rig;
+
+    public Rigidbody Rig
+    {
+        get {return rig; }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            rig.AddForce(Vector3.back, ForceMode.Impulse);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FPS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get{return instance;}
+    }
+
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddScore(int s)
+    {
+        score += s;
+    }
+}
