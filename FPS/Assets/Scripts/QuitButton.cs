802f30542432fed5f1416ffdd49547757aae78df
fedepvg <federicopvg@gmail.com>
2019-04-14 20:09:35 -0300

    game v1.0 finished

--- FPS/Assets/Scripts/QuitButton.cs
+++ FPS/Assets/Scripts/QuitButton.cs
@@ -5,17 +5,6 @@ using UnityEngine.UI;
 
 public class QuitButton : MonoBehaviour
 {
-    //[SerializeField]
-    //private Button button;
-
-    //void Awake()
-    //{
-    //    //if (button == null)
-    //    //    button = GetComponent<Button>();
-
-    //    //button.onClick.AddListener(OnClick);
-    //}
-
     public void QuitGame()
     {
         Application.Quit();
