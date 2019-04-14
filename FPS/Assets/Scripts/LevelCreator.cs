2fe340df92d5f243635c204892f6c526e889935c
fedepvg <federicopvg@gmail.com>
2019-04-14 17:04:04 -0300

    randomly generated map

---
+++ FPS/Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    public GameObject[] floorTemplate;
+    float templatesSize = 20f;
+    public int mapWidth;
+    public int mapHeight;
+    Vector3[,] floorPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorPos = new Vector3[mapWidth, mapHeight];
+        for(int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
+                floorPos[i, j] = Vector3.zero + new Vector3(templatesSize * i, 0, templatesSize * j);
+                Instantiate(floorTemplate[Random.Range(0, floorTemplate.Length)], floorPos[i, j], Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
